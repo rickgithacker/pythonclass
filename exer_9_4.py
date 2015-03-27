3f52d0454d13e41c0537dee767230cf47fbd86fe
rickgithacker <rick.okin@gmail.com>
2015-03-27 16:36:30 -0400

    chapter 9 files

---
+++ exer_9_4.py
@@ -0,0 +1,22 @@
+name = raw_input("Enter file:")
+if len(name) < 1 : name = "mbox-short.txt"
+handle = open(name)
+
+counts = dict()
+
+for line in handle:
+    if not line.startswith("From ") : continue
+    linelst = list()
+    linelst = line.rstrip().split()
+    counts[linelst[1]] = counts.get(linelst[1],0) + 1
+
+
+bigcount = None
+bigword = None
+
+for word,count in counts.items():
+    if bigcount is None or count > bigcount:
+        bigword = word
+        bigcount = count
+
+print bigword,bigcount
