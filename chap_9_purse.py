1721d5065c284f8451a263d89d0f94c4c3143c49
rickgithacker <rick.okin@gmail.com>
2015-03-27 11:35:01 -0400

    added chap_9 file

---
+++ chap_9_purse.py
@@ -0,0 +1,19 @@
+purse = dict()
+
+purse['money'] = 12
+purse['candy'] = 3
+purse['tissues'] = 75
+
+print purse
+
+print purse['candy']
+
+purse['candy'] = purse['candy'] + 2
+
+print purse
+
+jjj = {'chuck':1,'fred':42,'jan':100}
+
+print jjj
+
+print jjj['fred']
